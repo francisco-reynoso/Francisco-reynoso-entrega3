0d903a647f67787bc8378180de8d56dcfd4bf3be
francisco-reynoso <panchoreynoso251@gmail.com>
2023-09-28 22:22:06 -0300

    agregue cositas

--- scripts/comprar.js
+++ scripts/comprar.js
@@ -1,29 +1,33 @@
 const carrito = [];
-const miCarrito = document.getElementById("carrito-sin-notificacion");
+const miCarrito = document.querySelector("#carrito-sin-notificacion");
+const botones = document.querySelectorAll(".boton");
+
+botones.forEach((boton) => {
+    boton.addEventListener("click", () => {
+        comprar();
+
+        
+    });
+    
+});
 
 
-const boton = document.getElementById("1");
-boton.addEventListener("click", comprar);
 function comprar() {
     miCarrito.classList.remove("header-carrito");
     miCarrito.classList.add("header-carrito-con-notificacion");
 
-    const eleNotificacion = document.getElementsByClassName("sin-notificacion");
+    const eleNotificacion = document.querySelectorAll(".sin-notificacion");
 
-    for (const ele of eleNotificacion) {
+    for (const ele of eleNotificacion) { 
         ele.classList.remove("sin-notificacion");
         ele.classList.add("con-notificacion");
     }
-    
-
 }
-
 miCarrito.addEventListener("click", () => {
     miCarrito.classList.remove("header-carrito-con-notificacion");
     miCarrito.classList.add("header-carrito");
-    const eleNotificacion = document.getElementsByClassName("con-notificacion");
+    const eleNotificacion = document.querySelectorAll(".con-notificacion");
 
-    console.log(eleNotificacion)
     for (const ele of eleNotificacion) {
         ele.classList.remove("con-notificacion");
         ele.classList.add("sin-notificacion");
@@ -31,16 +35,11 @@ miCarrito.addEventListener("click", () => {
     }
 });
 
-const botones = document.querySelectorAll(".boton");
-
 console.log(botones)
-botones.forEach((boton) => {
-    boton.addEventListener("click", () => {
-        comprar();
-        
-    });
-    
-});
+
+
+
+
 
 // function comprar(e) {
 //     const idProducto = e.target.id;
